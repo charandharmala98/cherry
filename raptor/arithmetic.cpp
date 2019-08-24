be603f22688726d14904cc373a11dd78d32d7e88
charan_dharmala <cds4357@gmail.com>
2019-08-24 13:47:20 -0700

    "commit"

---
+++ raptor/arithmetic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? b;
+   ?? a;
+
+   raptor_prompt_variable_zzyz ="a";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> a;
+   raptor_prompt_variable_zzyz ="b";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> b;
+   cout << "Add"+(a+b)+"sub"+(a-b)+"mul"+(a*b)+"Div"+(a/b) << endl;
+   return 0;
+}
